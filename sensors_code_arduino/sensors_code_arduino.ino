360df6690c6c9e59b61cebdfca9f3a99476fbe70
Olaf Biegański <olaf.bieganski2001@gmail.com>
2023-04-03 20:37:52 +0200

    First trial code to test sensors

---
+++ sensors_code_arduino/sensors_code_arduino.ino
@@ -0,0 +1,49 @@
+#include <dht11.h>
+#include <Wire.h>
+#include <BH1750.h>
+
+dht11 DHT11;
+BH1750 lightMeter;
+
+#define DHT11PIN 2
+#define GROUND_SENSOR A0
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+  lightMeter.begin();
+  Serial.println(F("BH1750 Test"));
+  Serial.println("DHT11 TEST PROGRAM ");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT11LIB_VERSION);
+  Serial.println();
+}
+
+void loop()
+{
+  Serial.println("\n");
+
+  int chk = DHT11.read(DHT11PIN);
+
+  Serial.print("Read sensor: ");
+  /*switch (chk)
+  {
+    case DHTLIB_OK: 
+    Serial.println("OK"); 
+  }*/
+
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Temperature (°C): ");
+  Serial.println((float)DHT11.temperature, 2);
+
+  Serial.print("Ground humidity: ");
+  Serial.println(analogRead(GROUND_SENSOR));
+
+  Serial.print("Sunlihgt level (lux): ");
+  Serial.println(lightMeter.readLightLevel());
+
+  delay(1000);
+}
