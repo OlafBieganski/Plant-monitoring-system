570aceed680a81a9c1751d3f224dec97e309387a
Olaf Bieganski <olaf.bieganski2001@gmail.com>
2023-04-07 16:02:55 +0200

    Added layout and trial plant pic

--- PlantMonitoringSystemQt/mainwindow.cpp
+++ PlantMonitoringSystemQt/mainwindow.cpp
@@ -1,21 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPixmap>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)));
+    QPixmap plantPicture("/mnt/c/Users/olafb/OneDrive/Obrazy/roslina_zdrowa.png");
+    ui->label_plant_pic->setPixmap(plantPicture);
+    ui->label_plant_pic->setAlignment(Qt::AlignCenter);
+    ui->label_plant_state->setAlignment(Qt::AlignCenter);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-
-void MainWindow::on_pushButton_clicked()
-{
-    ui->label->setText("Button is clicked");
 }
